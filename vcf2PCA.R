debec0e54ff0868ad831c5596cff016d6bd341b8
Francisco Pina Martins <f.pinamartins@gmail.com>
2016-07-11 17:44:59 +0100

    Improved readablity and usability. Switched EV axes.

--- vcf2PCA.R
+++ vcf2PCA.R
@@ -2,18 +2,22 @@
 
 library ("SNPRelate")
 
+#Define inputs:
+vcf.fn <- "~/Desktop/sequenom/VCF/Qsuber_Sequenom.vcf"
+gds.fn <- "/tmp/file.gds"
+pops.fn <- "~/Desktop/sequenom/VCF/pop_names_sorted.txt"
+
 #input
-vcf.fn<-"~/Desktop/Qsuber_indiv90_miss80_maf06.recode.vcf"#load the data
-snpgdsVCF2GDS(vcf.fn,"test.gds",method="copy.num.of.ref")#import to class. use method= biallelic.only for biallelic snps
+snpgdsVCF2GDS(vcf.fn, gds.fn, method="copy.num.of.ref") # import to class. use method= biallelic.only for biallelic snps
 
 #summary of the vcf imported
-snpgdsSummary("test.gds")
+snpgdsSummary(gds.fn)
 
 #open the translated vcf imported
-genofile<-snpgdsOpen("test.gds")
+genofile<-snpgdsOpen(gds.fn)
 
 #import population names
-pop_code <- scan("~/Desktop/pops.txt", what=character())
+pop_code <- scan(pops.fn, what=character())
 
 #PCA
 pca<-snpgdsPCA(genofile)
@@ -28,12 +32,17 @@ sample.id<-read.gdsn(index.gdsn(genofile,"sample.id"))
 #data frame of eigenvectors
 tab<-data.frame(sample.id= pca$sample.id,
                 pop=factor(pop_code)[match(pca$sample.id, sample.id)],
-                EV1= pca$eigenvect[,1],# first eignvector (can be changed)
-                EV2= pca$eigenvect[,2],# the second eigenvector (can be changed)
+                EV1=pca$eigenvect[,1], # first eignvector (can be changed)
+                EV2=pca$eigenvect[,2], # the second eigenvector (can be changed)
                 stringsAsFactors=FALSE)
 
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)#creates space outside plot area
-plot(tab$EV2, tab$EV1,  col=as.integer(tab$pop), xlab="eigenvector 2",ylab="eigenvector 1", pch=as.integer(tab$pop))
-legend("topright", legend=levels(tab$pop),inset=c(-0.15,0), pch=1:nlevels(tab$pop), col=1:nlevels(tab$pop))#inset forces legend outside area
+par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE) # Creates space outside plot area
+plot(tab$EV1, tab$EV2, col=as.integer(tab$pop),
+     xlab="Eigenvector 1", ylab="Eigenvector 2",
+     pch=as.integer(tab$pop))
+
+legend("topright", legend=levels(tab$pop),
+       inset=c(-0.15,0),  # Inset forces legend outside area
+       pch=1:nlevels(tab$pop), col=1:nlevels(tab$pop))
 
 snpgdsClose(genofile)
